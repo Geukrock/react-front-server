a15a2620f117e796df730a29a6e1cdf08261e3cb
jeseungwook <jsw992@gmail.com>
2025-07-15 20:14:13 +0900

    환경 변수로 로컬, 서버 구분하여 서버 주소 추가

--- src/api/ProductApi.ts
+++ src/api/ProductApi.ts
@@ -1,16 +1,18 @@
 import axios from "axios";
 import { DetailImageUrl, Product, ProductDetail} from "types/product";
 
+const apiUrl = process.env.REACT_APP_API_URL;
 
 // 제품 전체 조회
 export async function fetchProducts(): Promise<Product[]> {
-    const response = await axios.get<Product[]>("http://localhost:8070/products");
+    console.log("TEST" + apiUrl);
+    const response = await axios.get<Product[]>(`${apiUrl}/products`);
     return response.data;
 }
 
 // 제품 단건 조회
 export async function deleteProduct(id: number): Promise<void> {
-    await axios.delete(`http://localhost:8070/products/${id}`);
+    await axios.delete(`${apiUrl}/products/${id}`);
 }
 
 // 제품 수정
@@ -23,30 +25,30 @@ export async function createProduct(formData: FormData) {
     const config = {
         headers: { "Content-Type": "multipart/form-data" },
     };
-    const response = await axios.post("http://localhost:8070/products", formData, config);
+    const response = await axios.post(`${apiUrl}/products`, formData, config);
     return response.data;
 }
 
 // 제품 상세 조회
 export async function fetchProductDetail(id:number) : Promise<ProductDetail> {
-    const response = await axios.get<ProductDetail>(`http://localhost:8070/products/${id}/with-detail-images`)
+    const response = await axios.get<ProductDetail>(`${apiUrl}/products/${id}/with-detail-images`)
     return response.data;
 }
 
 // 상세 이미지 조회
 export async function fetchDetailImageUrls(id:number) : Promise<DetailImageUrl[]>{
-    const response = await axios.get<DetailImageUrl[]>(`http://localhost:8070/products/${id}/detail-images`);
+    const response = await axios.get<DetailImageUrl[]>(`${apiUrl}/products/${id}/detail-images`);
     return response.data;
 }
 
 // 상세 이미지 추가(배열)
 export async function addDetailImages(id:number, formData: FormData) {
-    const response = await axios.post(`http://localhost:8070/products/${id}/detail-images`,formData);
+    const response = await axios.post(`${apiUrl}/products/${id}/detail-images`,formData);
     return response.data;
 }
 
 // 상세 이미지 삭제
 export async function deleteDetailImage(id:number){
-    await axios.delete(`http://localhost:8070/products/detail-images/${id}`);
+    await axios.delete(`${apiUrl}/products/detail-images/${id}`);
 };
 
